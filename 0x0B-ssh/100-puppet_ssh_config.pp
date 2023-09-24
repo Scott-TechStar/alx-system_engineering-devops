360140aa02c5ead12ee8a0e12cd34920af2d55d5
Scott-TechStar <johnniekips@gmail.com>
2023-09-23 21:10:00 -0700

    uppet config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Using puppet to make configuration changes to my server
+
+file{ 'etc/ssh/ssh_config':
+      ensure => present,
+
+      content =>"
+      	      # SSH client Configurations
+	      Host 34.227.91.107
+	      	   PasswordAuthentication no
+		   IdentityFile ~/.ssh/school
+		   BatchMode yes
+		   SendEnv LANG LC_*
+		   HashKnownHosts yes
+		   GSSAPIAuthentication yes
+		   GSSAPIDelegateCredentials no
+}
