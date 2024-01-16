bef5c686e0f936edaef3cf3541a91b809875f18c
John Mwadime <johnniekips@gmail.com>
2024-01-16 19:30:50 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""my first Reddit API request"""
-import requests
+# get subs
+from requests import get
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """the function return the number of subcribers"""
-    url = 'https://www.reddit.com/r/{}/.json'.format(subreddit)
-    header = {'User-Agent': 'Reddit API test'}
-
-    response = requests.get(url, headers=header, allow_redirects=False)
-    dict = response.json()
-    if dict.get("error", 200) == 404:
+    """subs"""
+    head = {'User-Agent': 'Dan Kazam'}
+    count = get('https://www.reddit.com/r/{}/about.json'.format(
+        subreddit), headers=head).json()
+    try:
+        return count.get('data').get('subscribers')
+    except:
         return 0
-    return dict.get("data").get("children")[0].get("data")\
-        .get("subreddit_subscribers")
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
